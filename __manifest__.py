494e82d95368abf794e5beaca128b0a4d2b262f6
onlyone-odoo <matiasb@onlyone.com.ar>
2025-05-17 14:40:49 -0300

    new branch 17.0

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://onlyone.odoo.com/",
     "license": "AGPL-3",
     "category": "Sales",
-    "version": "18.0.1.0.0",
+    "version": "17.0.1.0.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
